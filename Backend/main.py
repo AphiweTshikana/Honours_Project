9045a63b56e164cad3353b07bb322bd85e71e8a6
AphiweTshikana <phiwenaleditshikana@gmail.com>
2026-08-21 14:19:00 +0200

    Initial Commit

--- Backend/main.py
+++ Backend/main.py
@@ -19,10 +19,14 @@ app = FastAPI(title="Academic Tracking System API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://honours-project.vercel.app")
-],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://honours-project.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
